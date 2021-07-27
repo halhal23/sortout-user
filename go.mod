a994ea8f809b5953f812dbb8fd921c87a5b501df
halhal <hh.25799623@gmail.com>
2021-07-27 20:42:59 +0900

    add dockerfile

---
+++ go.mod
@@ -0,0 +1,5 @@
+module app
+
+go 1.16
+
+require github.com/halhal23/sortout-user v0.0.0-20210727112655-7c4ca012fdcb
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"sortout-user/domain"
+	"github.com/halhal23/sortout-user/domain"
 )
 
 func main() {
